db5a07ef6c17bfd8b7fdf0e237a71e9b0b75c3b5
DESKTOP-DRMB5P4\user1 <heesang522@gmail.com>
2026-09-14 17:18:48 +0900

    refector: AuthService.java 수정 이후 핸들러 기능 서비스로 옮겨야함

--- src/main/java/com/example/demo/service/AuthService.java
+++ src/main/java/com/example/demo/service/AuthService.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -35,6 +36,10 @@ public class AuthService {
         Authentication authentication = customAuthenticationProvider.authenticate(
                 UsernamePasswordAuthenticationToken.unauthenticated(memberId, memberPassword)
         );
+
+        // 💡 [핵심 추가] 현재 요청 쓰레드의 SecurityContext에 인증 정보 저장
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
         CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
         MemberDto memberDto = userDetails.getMemberDto();
         String role = memberDto.getMemberRole();
